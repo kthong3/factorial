7f0f780c0bf5166d399dc2464c1accfc5ba1951b
kthong3 <kthongsakounh@gmail.com>
2018-06-28 12:44:35 -0500

    solve factorials iteratively

--- factorial.rb
+++ factorial.rb
@@ -1,3 +1,15 @@
-def factorial
+def factorial(number)
+  if number == 0 || number == 1
+    return 1
+  end
 
+  i = 1
+  product = 1
+  until i == number
+    product = product * (i + 1)
+    i += 1
+  end
+  product
 end
+
+factorial(4)
